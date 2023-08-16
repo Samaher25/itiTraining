d41e91b4b84fa033cb5f48a9847e3f04648e96d1
Samaher Ahmed <ahmedsamaher3@gmail.com>
2023-08-16 21:29:22 +0300

    add database

--- lib/views/screens/page3.dart
+++ lib/views/screens/page3.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/models/todo_model.dart';
-import 'package:flutter_application_2/service/todo_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Page3 extends StatefulWidget {
   const Page3({super.key});
@@ -10,11 +9,32 @@ class Page3 extends StatefulWidget {
 }
 
 class _Page3State extends State<Page3> {
-  
+  final Stream<QuerySnapshot> petsStream = FirebaseFirestore.instance.collection('pets').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("page3"),
+    return StreamBuilder<QuerySnapshot>(
+      stream: petsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),);
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['name']),
+              subtitle: Text(data['gender']),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
